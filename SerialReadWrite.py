2aa9d4661ca0ea81f595d151f94222c5bfdee94a
pinaxe1 <pinaxe@gmail.com>
2019-04-04 15:52:15 -0400

    Update SerialReadWrite.py

--- SerialReadWrite.py
+++ SerialReadWrite.py
@@ -10,5 +10,4 @@ while True:
      ser.write(str(chr(counter))) # Convert the decimal number to ASCII then send it to the Arduino
      print ser.readline() # Read the newest output from the Arduino
      sleep(.1) # Delay for one tenth of a second
-     if counter == 255:
-     counter = 32
+     if counter == 255: counter = 32
